f5f197ce33cda570cd07f7f642bc272014b05922
Maina.dev <138670482+Mainaaa1@users.noreply.github.com>
2026-01-12 14:34:33 +0300

    Implement BankAccount class with basic methods

---
+++ day-01-oop/classes/BankAccount.java
@@ -0,0 +1,26 @@
+public class BankAccount {
+
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber) {
+        this.accountNumber = accountNumber;
+        this.balance = 0.0;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
