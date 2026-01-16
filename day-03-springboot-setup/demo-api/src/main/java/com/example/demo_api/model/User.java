a3c5c99ae92f41be6100273b01201ac2def0946b
Maina.dev <138670482+Mainaaa1@users.noreply.github.com>
2026-01-16 19:52:54 +0300

    Update User.java

--- day-03-springboot-setup/demo-api/src/main/java/com/example/demo_api/model/User.java
+++ day-03-springboot-setup/demo-api/src/main/java/com/example/demo_api/model/User.java
@@ -2,27 +2,29 @@
 ```java
 package com.example.demoapi.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
 
     public User() {
     }
 
-    public User(Long id, String name, String email) {
-        this.id = id;
+    public User(String name, String email) {
         this.name = name;
         this.email = email;
     }
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
